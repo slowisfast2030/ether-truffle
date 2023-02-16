1e4cf107b5ce99c068db8123b7fb6841279b02a9
slowisfast <slowisfast2030@163.com>
2023-02-16 23:00:46 +0800

    all is well

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.5.13;
 
 contract MyContract {
-  event Log(address indexed addr);
+  event Log1(address indexed addr);
+  event Log2(address addr);
 
   constructor() public{
-    emit Log(address(this));
+    emit Log1(address(this));
+    emit Log2(address(this));
   }
 
   function add(uint256 a, uint256 b) public pure returns (uint256) {
