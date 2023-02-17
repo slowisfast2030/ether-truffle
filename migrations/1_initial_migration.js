1bd702615671c91732d3ad13a507ae74ae8927d1
slowisfast2030 <slowisfast2030@163.com>
2023-02-17 11:49:36 +0800

    all is well

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,9 @@
 var MyContract = artifacts.require("MyContract");
 var Impossible = artifacts.require("Impossible");
+var AnotherContract = artifacts.require("AnotherContract");
 
 module.exports = function(deployer) {
   deployer.deploy(MyContract);
   deployer.deploy(Impossible);
+  deployer.deploy(AnotherContract);
 };
